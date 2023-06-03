62c6ff040ed82cf5088d7ad6fcd5c1e071c9d6de
Amitraj408 <dynamicamitraj@gmail.com>
2023-06-03 10:07:03 +0530

    added welcome controller

---
+++ springbootCurdOperation/src/main/java/com/neosoft/springbootCurdOperation/Controller/AccountController.java
@@ -0,0 +1,12 @@
+package com.neosoft.springbootCurdOperation.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AccountController {
+    @GetMapping("/welcome")
+    public String getWelcomeMessage(){
+        return "welcome to our project";
+    }
+}
